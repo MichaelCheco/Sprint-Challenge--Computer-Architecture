31afca8117a1e61cca5b4dc20090795f7fa1158e
Michael <checomichael2@gmail.com>
2019-07-26 12:09:21 -0400

    implement required instructions

--- ls8.py
+++ ls8.py
@@ -5,6 +5,15 @@
 import sys
 from cpu import *
 
+args = sys.argv
+
+if len(args) < 2:
+    print("Please provide the path of the file")
+    sys.exit(1)
+
+program = args[1]
+
 cpu = CPU()
-cpu.load()
+
+cpu.load(program)
 cpu.run()
